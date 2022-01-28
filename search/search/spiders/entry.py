aa8a3e0a9ca90c419352b4ea878de23b601d4201
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-01-28 10:50:52 +0900

    search hoge

--- search/search/spiders/entry.py
+++ search/search/spiders/entry.py
@@ -2,9 +2,13 @@ import scrapy
 
 
 class EntrySpider(scrapy.Spider):
-    name = 'entry'
-    allowed_domains = ['google.com']
-    start_urls = ['http://google.com/']
+    name = "entry"
+    allowed_domains = ["google.com"]
+    def __init__(self, query="", *args, **kwargs):
+        super(EntrySpider, self).__init__(*args, **kwargs)
+        self.start_urls = [f"http://google.com/search?q={query}"]
+        print(f"{query=}")
+
 
     def parse(self, response):
-        pass
+        print(response.body)
