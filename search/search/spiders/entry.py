40e59dcaf81507531af50d39a52bd06511c74021
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-01-28 13:42:43 +0900

    follow search result

--- search/search/spiders/entry.py
+++ search/search/spiders/entry.py
@@ -4,11 +4,17 @@ import scrapy
 class EntrySpider(scrapy.Spider):
     name = "entry"
     allowed_domains = ["google.com"]
+
     def __init__(self, query="", *args, **kwargs):
         super(EntrySpider, self).__init__(*args, **kwargs)
         self.start_urls = [f"http://google.com/search?q={query}"]
-        print(f"{query=}")
-
 
     def parse(self, response):
-        print(response.body)
+        xpath = "//a/parent::div[contains(@class, 'kCrYT')]"
+        nodes = response.xpath(xpath)
+        for node in nodes:
+            url = node.xpath("a").attrib["href"]
+            yield response.follow(url, self.print_response)
+
+    def print_response(self, response):
+        print(response)
