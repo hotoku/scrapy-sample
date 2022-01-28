f47297de740475aaf620925ad1e15e444c742916
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-01-28 16:37:57 +0900

    this runs

--- search/search/pipelines.py
+++ search/search/pipelines.py
@@ -1,11 +1,47 @@
-import sqlite3
+from __future__ import annotations
+from dataclasses import dataclass
+from datetime import datetime
+
+from sqlitedict import SqliteDict
+import scrapy
 
 from .items import SearchItem
 
 
+@dataclass
+class Key:
+    keyword: str
+    rank: int
+
+    def to_str(self) -> str:
+        return f"{self.rank}/{self.keyword}"
+
+    @staticmethod
+    def parse(s: str) -> Key:
+        n = s.find("/")
+        keyword = s[(n+1):]
+        rank = int(s[:n])
+        return Key(keyword, rank)
+
+
+@dataclass
+class Value:
+    title: str
+    url: str
+    timestamp: datetime
+
+
 class SaveToSqlitePipeline:
-    def process_item(self, item, spider):
-        if isinstance(item, SearchItem):
-            con = sqlite3.connect("db.sqlite")
+    def process_item(self, item: scrapy.Item, spider: scrapy.Spider):
+        if not isinstance(item, SearchItem):
             return item
+        with SqliteDict("db.sqlite", autocommit=True) as dic:
+            key = Key(item["keyword"], item["rank"]).to_str()
+            value = Value(item["title"], item["url"], item["timestamp"])
+            if not key in dic:
+                ret = []
+            else:
+                ret = dic[key]
+            ret.append(value)
+            dic[key] = ret
         return item
